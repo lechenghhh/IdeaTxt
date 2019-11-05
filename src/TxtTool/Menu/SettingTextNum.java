8c170f8b08f5a63eca8d9d3eb5bf4c9bc88cf5a1
lechenggood@foxmail.com <imgh6982357>
2019-11-05 11:30:43 +0800

    无修改

--- src/TxtTool/Menu/SettingTextNum.java
+++ src/TxtTool/Menu/SettingTextNum.java
@@ -5,8 +5,16 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+
+import javax.swing.JPanel;
 
 import TxtTool.Utils.RandomAccessFileUtils.TxtReader;
+
+import static TxtTool.Action.NextPage.TOOL_WINDOW_ID;
 
 /*测试程序*/
 public class SettingTextNum extends AnAction {
@@ -22,5 +30,10 @@ public class SettingTextNum extends AnAction {
         SettingConfig.instance().setTextNum(Integer.parseInt(textNum));
         TxtReader.instance().init(SettingConfig.instance().getFilePath());
 
+//        ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TOOL_WINDOW_ID);
+//        JPanel container = (JPanel) toolWindow.getContentManager().getContent(0).getComponent();
+//        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+//        Content content = contentFactory.createContent(container, "Control", false);
+//        toolWindow.getContentManager().addContent(content);
     }
 }
